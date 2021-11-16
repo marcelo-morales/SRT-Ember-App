9ddc7709a9650445a88245071365be5cdafa7a1a
Bointeka <jkaoga@gmail.com>
2021-11-16 18:10:33 -0500

    Added the options required to create commands and arranged them in a possible arrangement

--- app/controllers/srt.js
+++ app/controllers/srt.js
@@ -1,11 +1,31 @@
-import Component from "@glimmer/component";
+import Controller from "@ember/controller";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 
-export default class SRT extends Component {
-    @tracked record;
+export default class SrtController extends Controller {
+    //@tracked record;
+    @tracked is25Scan;
+    @tracked az;
+    @tracked el;
+    @tracked glat;
+    @tracked glon;
+    @tracked azoff;
+    @tracked eloff;
+    @tracked freq;
 
-    @action startCommands() {
+    @action toggle25Point() {
+        this.is25Scan = true;
+    }
 
+    @action toggleBeamswitch(){
+        this.is25Scan = false;
+    }
+
+    @action submitCommands() {
+        
+    }
+
+    @action validateGalactic() {
+        
     }
 };
