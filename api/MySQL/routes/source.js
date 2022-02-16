1d06cfbfa17d1955fd8855cfc55baa3bd879d10d
Bointeka <jkaoga@gmail.com>
2022-02-15 22:15:56 -0500

    Added serializer that allowed for fetched API data to be stored in Ember store

--- api/MySQL/routes/source.js
+++ api/MySQL/routes/source.js
@@ -1,18 +1,17 @@
-require("dotenv").config();
-const express = require("express");
-const ApiError = require("../models/ApiError");
-const SourceDao = require("../data/SourceDao");
+require('dotenv').config();
+const express = require('express');
+const ApiError = require('../models/ApiError');
+const SourceDao = require('../data/SourceDao');
 const source = new SourceDao();
 const router = express.Router();
 
-
-router.get("/api/sources", async (req, res, next) => {
-    try{
-        const data = await source.readAll();
-        res.status(200).json({data});
-    } catch (err) {
-        next(err);
-    }
+router.get('/api/sourcelists', async (req, res, next) => {
+  try {
+    const data = await source.readAll();
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
---
+++ app/serializers/sourcelist.js
@@ -0,0 +1,29 @@
+import JSONAPISerializer from '@ember-data/serializer/json-api';
+
+export default class sourcelistSerializer extends JSONAPISerializer {
+  normalizeFindAllResponse(store, primaryModelClass, payload, id, requestType) {
+    let count = 1;
+    let data = {
+        "id": count.toString(),
+        "type": primaryModelClass.modelName,
+        "relationships": {
+            "source": {
+                "data": []
+            } 
+        }};
+    payload.results.forEach((source) => {
+        let sourceData = {
+            type: "source",
+            id: count.toString(),
+            attributes: {
+                "source": source.sources
+            }
+        };
+        data.relationships.source.data.push(sourceData);
+        count++;
+    });
+    payload["data"] = data;
+    delete payload.results;
+    return super.normalizeFindAllResponse(...arguments);
+  }
+}
