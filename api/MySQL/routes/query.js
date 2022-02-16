2d48198b3e0da4a9d182cad8aef6f7a02a48aaa8
Bointeka <jkaoga@gmail.com>
2022-02-15 23:30:13 -0500

    Simplified API to be more consistent

--- api/MySQL/routes/query.js
+++ api/MySQL/routes/query.js
@@ -5,9 +5,7 @@ const ResultsDao = require("../data/ResultsDao");
 const SourceDao = require("../data/SourceDao");
 const ApiError = require("../models/ApiError");
 const query = new QueryDao();
-const source = new SourceDao();
 const router = express.Router();
-
 
 //QUERY TABLE API
 router.get("/api/queries/:id", async (req, res, next) => {
@@ -16,58 +14,40 @@ router.get("/api/queries/:id", async (req, res, next) => {
         if (isNaN(id)) {
             throw new ApiError(400, "The id needs to be a number");
         }
-        const data = await query.read(id, queriesTable);
-        res.status(200).json({data});
+        const data = await query.read(id);
+        res.status(200).json({ data });
     } catch (err) {
         next(err);
     }
 });
-
 
 router.post("/api/queries", async (req, res, next) => {
     try {
-        let model = undefined;
-        let { command, email } = req.body
-        if (command === undefined) {
-            model = req.body;
-            command = model.data.attributes.command;
-            email = model.data.attributes.email;
-        }
-        if (command === undefined || command === "" ) {
+        let { command, email } = req.body;
+        if (command === undefined || command === "") {
             throw new ApiError(400, "Require command string");
         }
+        email = "email";
         if (email === undefined || email === "") {
             throw new ApiError(400, "Require email to submit command");
         }
-
-        
-        const data = await query.create(command, email, queriesTable);
-        if (model !== undefined) {
-            model.data.attributes.dbid = "2";
-            model.data.id = data._id;
-            res.status(200).json(model);
-        } else {
-            res.status(200).json({data});
-        }
+        let data = await query.create(command, email);
+        data["command"] = command;
+        data["email"] = email;
+        res.status(200).json({ data });
     } catch (err) {
         next(err);
     }
 });
-
+    
 router.delete("/api/queries/:id", async (req, res, next) => {
     try {
         const { id } = req.params;
-        const data = await query.delete(id, queriesTable);
-        res.status(200).json({data});
+        const data = await query.delete(id);
+        res.status(200).json({ data });
     } catch (err) {
         next(err);
     }
 });
-
-
-
-
-
-
 
 module.exports = router;
--- api/MySQL/routes/results.js
+++ api/MySQL/routes/results.js
@@ -1,73 +1,61 @@
-require("dotenv").config();
-const express = require("express");
-const ApiError = require("../models/ApiError");
-const ResultsDao = require("../data/ResultsDao");
+require('dotenv').config();
+const express = require('express');
+const ApiError = require('../models/ApiError');
+const ResultsDao = require('../data/ResultsDao');
 const results = new ResultsDao();
 const router = express.Router();
 
 //Reads either entire results or results filtered by command
-router.get("/api/results", async (req, res, next) => {
+router.get('/api/results', async (req, res, next) => {
     try {
         const { command } = req.body;
-        if (command === undefined || command === "") {
+        if (command === undefined || command === '') {
             const data = await results.readAll();
-            res.status(200).json({data});
+            res.status(200).json({ data });
         } else {
             const data = await results.read(command);
-            res.status(200).json({data});
+            res.status(200).json({ data });
         }
     } catch (err) {
         next(err);
     }
 });
 
-
-
-router.post("/api/results", async (req, res, next) => {
+router.post('/api/results', async (req, res, next) => {
     try {
         let model = undefined;
-        let { command } = req.body
-        if (command === undefined) {
-            model = req.body;
-            command = model.data.attributes.command;
+        let { command } = req.body;
+        if (command === undefined || command === '') {
+            throw new ApiError(400, 'Require command string');
         }
-        if (command === undefined || command === "") {
-            throw new ApiError(400, "Require command string");
-        }
-        
+
         const data = await results.create(command);
-        if (model !== undefined) {
-            model.data.attributes.dbid = "2";
-            model.data.id = data._id;
-            res.status(200).json(model);
-        } else {
-            res.status(200).json({data});
-        }
+        res.status(200).json({ data });
     } catch (err) {
         next(err);
     }
 });
 
-router.put("/api/results/:id", async (req, res, next) => {
+router.put('/api/results/:id', async (req, res, next) => {
     try {
         const { id } = req.params;
         let { data, image } = req.body;
 
         if (data === undefined && image === undefined) {
-            throw new ApiError(400, "Require values inorder to update");
+            throw new ApiError(400, 'Require values inorder to update');
         }
-        data = await results.update(id, { data, image});
-        res.status(200).json({data});
+        data = await results.update(id, { data, image });
+        res.status(200).json({ data });
     } catch (err) {
         next(err);
     }
 });
 
-router.delete("/api/results/:id", async (req, res, next) => {
+router.delete('/api/results/:id', async (req, res, next) => {
     try {
         const { id } = req.params;
         const data = await results.delete(id);
-        res.status(200).json({data});
+        res.status(200).json({ data });
     } catch (err) {
         next(err);
     }
