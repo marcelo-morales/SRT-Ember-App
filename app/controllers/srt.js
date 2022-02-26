5f003a03bc8a093d54dc8ee76ab42a07d9a6e9c1
Bointeka <jkaoga@gmail.com>
2022-02-26 00:44:37 -0500

    Changed how commands are added and submitted. Simplified by making it all accessible

--- app/controllers/srt.js
+++ app/controllers/srt.js
@@ -8,18 +8,13 @@ export default class SrtController extends Controller {
   idCount = 1;
   //@tracked file;
   @tracked prevCommand = ['', ''];
-  @tracked command = '';
-  @tracked advancedSettings = false;
+  @tracked command = ": record";
 
-  //Setup
-  @tracked setup = true; //Check if setup is not filled
   @tracked sourceName = undefined;
-  @tracked is25Scan = undefined;
+  @tracked is25Scan = true;
   @tracked freq = undefined;
-  //Record after setup
   @tracked recordTime;
 
-  //advanced Settings
   @tracked az;
   @tracked el;
   @tracked glat;
@@ -27,36 +22,45 @@ export default class SrtController extends Controller {
   @tracked azoff;
   @tracked eloff;
 
-  @action toggleAdvanced() {
-    this.advancedSettings = !this.advancedSettings;
-  }
-
   @action toggle25Point() {
     this.is25Scan = true;
-    this.validateSetup();
   }
 
   @action toggleBeamswitch() {
     this.is25Scan = false;
-    this.validateSetup();
   }
 
-  //Setup methods
-  @action selectSetupSource(source) {
-    this.sourceName = source;
-    this.validateSetup();
-  }
-
-  @action validateSetup() {
-    if (
-      this.sourceName !== "" &&
-      this.is25Scan !== undefined &&
-      this.freq !== undefined
-    ) {
-      this.command = `: ${this.sourceName}\n: mode ${this.getMode()}\n: freq ${
-        this.freq
-      }\n: record name`;
+  @action addAzEl() {
+    if (this.el === undefined || this.az === undefined) {
+      return;
     }
+    this.prevCommand = this.command;
+    let azelCommand = `\n: azel ${this.az} ${this.el}`;
+    this.command = this.command.concat(azelCommand);
+  }
+
+  @action addFrequency(){
+    if (this.freq === undefined) {
+      return;
+    }
+    this.prevCommand = this.command;
+    let freqCommand = `\n: freq ${this.freq}`;
+    this.command = this.command.concat(freqCommand);
+  }
+
+  @action addGalactic(){
+    if (this.glat === undefined || this.glon === undefined) {
+      return;
+    }
+    this.prevCommand = this.command;
+    let galCommand = `\n: galactic ${this.glat} ${this.glon}`;
+    this.command = this.command.concat(galCommand);      
+  }
+
+  @action addMode(){
+    this.prevCommand = this.command;
+    let modeCommand = `\n: mode ${this.getMode()}`;
+    this.command = this.command.concat(modeCommand);      
   }
 
   getMode() {
@@ -67,105 +71,64 @@ export default class SrtController extends Controller {
     }
   }
 
-  //Basic settings
-  @action validateTime() {
-    if (this.recordTime !== undefined) {
-      this.setup = false;
+  @action addOffset() {
+    if (this.azoff === undefined || this.eloff === undefined) {
+      return;
     }
+    this.prevCommand = this.command;
+    let offsetCommand = `\n: offset ${this.azoff} ${this.eloff}`;
+    this.command = this.command.concat(offsetCommand);
+  }
+
+  @action addSource(){
+    if (this.sourceName === undefined) {
+      return;
+    }
+    this.prevCommand = this.command;
+    let sourceCommand = `\n: ${this.sourceName}`;
+    this.command = this.command.concat(sourceCommand);      
+  }
+
+  @action addTime(){
+    if (this.recordTime === undefined) {
+      return;
+    }
+    this.prevCommand = this.command;
+    let timeCommand = `\n:${this.recordTime}`;
+    this.command = this.command.concat(timeCommand);      
   }
 
   @action selectSource(source) {
     this.sourceName = source;
   }
 
-  @action submitBasicCommands() {
-    this.prevCommand = this.command;
-    let command = `\n:${this.recordTime} ${this.sourceName}`;
-    this.command = this.command.concat(command);
-    this.setup = true;
-    this.recordTime = undefined;
-  }
-
-  @action submitAdvancedCommmands() {
-    this.prevCommand = this.command;
-    let offsetCommand = `\n: offset ${this.azoff} ${this.eloff}`;
-    let galCommand = `\n: galactic ${this.glat} ${this.glon}`;
-    let azelCommand = `\n: azel ${this.az} ${this.el}`;
-
-    this.command = this.command.concat(azelCommand);
-    this.command = this.command.concat(galCommand);
-    this.command = this.command.concat(offsetCommand);
-
-    this.resetAdvanced();
-    this.setup = true;
-    this.recordTime = undefined;
-  }
-
-  @action validateAZ() {
-    if (isNaN(parseInt(this.az, 10))) {
-      this.az = undefined;
-    }
-    if (this.az < 0) {
-      this.az = 0;
-    } else if (this.az > 355) {
-      this.az = 355;
-    }
-    this.validateAdvanced();
-  }
-
-  @action validateEL() {
-    if (isNaN(parseInt(this.el, 10))) {
-      this.el = undefined;
-    }
-    if (this.el < 0) {
-      this.el = 0;
-    } else if (this.el > 89.0) {
-      this.el = 89.0;
-    }
-    this.validateAdvanced();
-  }
-
-  @action validateAdvanced() {
-    if (
-      this.azoff === undefined ||
-      this.eloff === undefined ||
-      this.glat === undefined ||
-      this.glon === undefined ||
-      this.az === undefined ||
-      this.el === undefined
-    ) {
-      console.log('flipped');
-      this.setup = true;
-    } else {
-      console.log('not flip');
-      this.setup = false;
-      console.log(this.setup);
-    }
-  }
-
   //TODO: Check if command is duplicate
-  @action async submitCommand() {
-    this.command = this.command.concat('\n: stow\n: vplot');
-    let query = this.store.createRecord('query', {
-      command: this.command,
-      id: this.idCount,
-      email: this.email
-    });
-    try {
-      await query.save();
-    } catch (error) {
-      console.error(error);
+    @action async submitCommand() {
+        if (this.email === undefined || this.command === undefined) {
+            return;
+            //Need to display error
+        }
+        try {
+                if (!this.command.includes("record")){
+                    throw new Error("Command needs to be recorded");
+                }  
+                if (this.command.indexOf("record") === this.command.length - 6) {
+                    throw new Error("Recorded command cannot be empty");
+                }
+                this.command = this.command.concat('\n: roff\n: stow\n: vplot');
+                let query = this.store.createRecord('query', {
+                    command: this.command,
+                    id: this.idCount,
+                    email: this.email
+                });
+                try {
+                    await query.save();
+                } catch (error) {
+                    console.error(error);
+                }
+                this.idCount++;
+        } catch (error) {
+            console.error(error);
+        }
     }
-    this.idCount++;
-  }
-
-  resetAdvanced() {
-    console.log('resetting');
-    this.az = undefined;
-    this.el = undefined;
-    this.glat = undefined;
-    this.glon = undefined;
-    this.azoff = undefined;
-    this.eloff = undefined;
-  }
 }
